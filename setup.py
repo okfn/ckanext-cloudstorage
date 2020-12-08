eb8569229d35132d40c4faf318a9c01b70e5d3f5
chris48s <chris.shaw480@gmail.com>
2020-12-08 14:38:26 +0000

    0.2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ckanext-cloudstorage',
-    version='0.1.1',
+    version='0.2.1',
     description='Cloud storage for CKAN',
     classifiers=[],
     keywords='',
